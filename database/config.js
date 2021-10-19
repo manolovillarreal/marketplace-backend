6e138c1ecb3ceb13003564bda701b34a84eec362
Manuel Villarreal <manolovillarreal@hotmail.com>
2021-10-19 01:21:23 -0500

    Se establecio el ambiente de pruebas

--- database/config.js
+++ database/config.js
@@ -1,23 +1,21 @@
 const mongoose = require('mongoose');
+const { envs } = require('../Types/types');
 
 
 const getConnectionString = ()=>{
 
     switch (process.env.NODE_ENV) {
-        case envs.TEST: return  process.env.MONGODB_CNN;
-        case envs.DEV: return  process.env.MONGODB_CNN;
+        case envs.TEST: return  process.env.MONGODB_CNN_TEST;
+        case envs.DEV: return  process.env.MONGODB_CNN_DEV;
         case envs.PROD: return  process.env.MONGODB_CNN;
         default: return  process.env.MONGODB_CNN;
-    
-        default:
-            break;
     }
 }
 const dbConnection = async() => {
 
     try {
-
-        await mongoose.connect( getConnectionString(), {
+        const cnn =  getConnectionString();
+        await mongoose.connect( cnn, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
